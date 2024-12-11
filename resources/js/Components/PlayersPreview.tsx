fd21cd19443fc9fca05a210271cae4713663f851
Saul Montilla <smontilla@ubiipagos.com>
2024-12-11 14:19:39 -0400

    No permitir request mientras esta cargando

--- resources/js/Components/PlayersPreview.tsx
+++ resources/js/Components/PlayersPreview.tsx
@@ -34,6 +34,10 @@ export default function PlayersPreview() {
     }, [])
 
     function handleClick() {
+        if (loading) {
+            return
+        }
+
         setLoading(true)
 
         setTimeout(() => {
